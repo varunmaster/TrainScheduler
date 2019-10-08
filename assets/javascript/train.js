cb7109c2b31a760bcffa3b5230313bb20464ef94
Varun Master <varunmaster95@gmail.com>
2019-10-07 22:17:40 -0400

    added html and some js. TODO: remove the hardcoded table and use jq/fire to dynamically populate

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,1 +1,23 @@
-var moment = require('moment');
+// var moment = require('moment');
+
+var config = {
+    apiKey: "AIzaSyA80R6FtX0jub45y6Do_EmN8V79l3b3tQ4",
+    authDomain: "varun-bootcamp.firebaseapp.com",
+    databaseURL: "https://varun-bootcamp.firebaseio.com",
+    projectId: "varun-bootcamp",
+    storageBucket: ""
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+//put the code for getting the values from the user form and put the name, dest, freq, away, next arrival, etc. here and dump it to fire
+database.ref().set({
+    trainName: "Never on Time Transit"
+  });
+
+database.ref().on("value", function (snapshot) {
+    console.log(snapshot.val());
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
